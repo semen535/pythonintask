df23729bd556680cbf859d97e70b00fbaf686376
Gevenov S.A <gevenov98.98@mail.ru>
2017-04-19 16:56:09 +0300

    Гевенов С.А. Задача 4, Вариант 7

---
+++ ist/Gevenov_Semen/task_4_7.py
@@ -0,0 +1,16 @@
+# Задача 4. Вариант 7.
+# Напишите	программу,	которая	выводит	имя,	под	которым	скрывается	Мария	Луиза Чеччарелли.
+
+# Gevenov S. A.
+# 16.04.2017
+
+interes="Моника Витти - итальянская актриса" 
+print("Мария Луиза Чеччарелли более известна, как ", interes) 
+PlaceOfBirth="Рим, Италия" 
+print("Место рождения: ", PlaceOfBirth) 
+YearOfBirth=("1931") 
+YearOfBirth=int(YearOfBirth) 
+print("Год рождения: ", YearOfBirth) 
+Age=int("2017")-YearOfBirth 
+print("Возраст: ", Age) 
+input("\n\nНажмите Enter для выхода.")
